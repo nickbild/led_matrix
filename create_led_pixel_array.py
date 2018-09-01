1252380fb2b40e470af52dd0d6823771a39b87b6
nickbild <nick.bild@gmail.com>
2018-09-01 19:50:02 -0400

    Fine tune brightness, add utility to generate data arrays, add example data.

---
+++ create_led_pixel_array.py
@@ -0,0 +1,47 @@
+import sys
+
+red = ""
+green = ""
+blue = ""
+
+for line in open(sys.argv[1]):
+	line = line.strip()
+	line = line.replace(" ", "")
+	lineAry = line.split(",")
+
+	red += "{"
+	green += "{"
+	blue += "{"
+
+	cnt = 1
+	for pixel in lineAry:
+		if pixel == "":
+			continue
+
+		b = str(int(int(pixel[4:6], 16) / 10.2))
+		g = str(int(int(pixel[6:8], 16) / 10.2))
+		r = str(int(int(pixel[8:10], 16) / 10.2))
+
+		red += r
+		green += g
+		blue += b
+
+		if cnt < 32:
+			red += ","
+	                green += ","
+        	        blue += ","
+
+
+		cnt += 1
+
+
+	red += "},\n"
+        green += "},\n"
+        blue += "},\n"
+
+print red
+print "----"
+print green
+print "----"
+print blue
+
